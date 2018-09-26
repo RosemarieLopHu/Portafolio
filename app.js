d100d8600ef0d7d7bfc015532ccd8893e3b94cdc
RosemarieLopezH <rosemarie61@gmail.com>
2018-09-26 11:50:41 -0500

    añadiendo archivos

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-$('body').scrollspy({ target: '#main-nav' })
+/* $(document).ready(function () {
+    $('.sidenav').sidenav();
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.parallax');
+    var instances = M.Parallax.init(elems, options);
+  });
+ */
